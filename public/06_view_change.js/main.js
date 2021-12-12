61841f8e69c5bd3418a34e9f006fda727ceff411
bluepost59 <kaput.kat@gmail.com>
2021-12-12 17:30:03 +0900

    ポカミスを直した（onclickで関数をうっかりcallすると毎度loadした瞬間実行されてしまう）

--- public/06_view_change.js/main.js
+++ public/06_view_change.js/main.js
@@ -2,9 +2,19 @@ window.onload = () => {
     const main_div = document.getElementById("main_screen");
     const change_button = document.getElementById("change_button");
 
-    change_button.onclick = change_screen();
+    main_div.innerText = "This is first page.";
+
+    change_button.onclick = change_screen;
 }
 
 function change_screen() {
+    console.log("change screen");
+    const main_div = document.getElementById("main_screen");
+
+    while (main_div.firstChild) {
+        main_div.removeChild(main_div.firstChild);
+    }
+
+    main_div.innerText = "This is 2nd page.";
 
 }
