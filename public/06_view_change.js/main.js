24a18e6fbd67c0ae8b2d209e9f6aab587c1da4a2
bluepost59 <kaput.kat@gmail.com>
2021-12-12 17:19:06 +0900

    いろいろ練習

---
+++ public/06_view_change.js/main.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    const main_div = document.getElementById("main_screen");
+    const change_button = document.getElementById("change_button");
+
+    change_button.onclick = change_screen();
+}
+
+function change_screen() {
+
+}
